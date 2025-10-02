96a0ce263b0470e8982c63113892c39930c5c50f
phamquangnam2005-prog <phamquangnam2005@gmail.com>
2025-10-02 23:37:22 +0700

    ok

--- resources/views/layout/cart_layout.blade.php
+++ resources/views/layout/cart_layout.blade.php
@@ -579,12 +579,12 @@
     <!-- JavaScript Libraries -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/wow/wow.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="{{ asset('lib/wow/wow.min.js') }}"></script>
+    <script src="{{ asset('public/client/lib/owlcarousel/owl.carousel.min.js') }}"></script>
 
 
     <!-- Template Javascript -->
-    <script src="js/main.js"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
 </body>
 
 </html>
